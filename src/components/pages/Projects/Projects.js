a10fb34f2d4fcf39a364451044025ddd78d65893
Victor Akindele <105135998+hurlavic@users.noreply.github.com>
2023-03-14 21:56:13 +0000

    update to project page

--- src/components/pages/Projects/Projects.js
+++ src/components/pages/Projects/Projects.js
@@ -4,24 +4,32 @@ import "./projects.css";
 function ProjectGallery(props) {
   return (
     <section id="project">
-      <div class="cardContainer col-lg-4 col-md-6 col-sm-12">
-        <div class="card">
-          <img src={props.image} class="card-img-top" alt={props.title} />
-          <div class="card-body">
-            <h3 class="card-title">{props.title}</h3>
-            <p class="card-text">{props.description}</p>
-            {props.deploy && (
-              <a href={props.deploy} rel="noopener noreferrer" target="_blank">
-                <button type="button" class="btn btn-primary">
-                  Visit Site
-                </button>
-              </a>
-            )}
-            <a href={props.repo} rel="noopener noreferrer" target="_blank">
-              <button type="button" class="btn btn-secondary">
-                View Codes
-              </button>
-            </a>
+      <div class="container-fluid row-1">
+        <div class="row row-cols-1 row-cols-md-2 ">
+          <div class="cardContainer col-lg-4 col-md-6 col-sm-12">
+            <div class="card">
+              <img src={props.image} class="card-img-top" alt={props.title} />
+              <div class="card-body">
+                <h3 class="card-title">{props.title}</h3>
+                <p class="card-text">{props.description}</p>
+                {props.deploy && (
+                  <a
+                    href={props.deploy}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    <button type="button" class="btn btn-primary">
+                      Visit Site
+                    </button>
+                  </a>
+                )}
+                <a href={props.repo} rel="noopener noreferrer" target="_blank">
+                  <button type="button" class="btn btn-secondary">
+                    View Codes
+                  </button>
+                </a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
