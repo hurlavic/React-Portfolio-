6b581599f85561c0d34a689fa91e7a8987fb8542
Victor Akindele <105135998+hurlavic@users.noreply.github.com>
2023-03-14 02:42:30 +0000

    added about page

--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -1,17 +1,56 @@
 import React from "react";
+import "./about.css";
+import { HiBadgeCheck } from "react-icons/hi";
 
 function About() {
   return (
-    <div>
-      <h1>About Page</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam. Integer gravida dui
-        mauris, ut interdum nunc egestas sed. Aenean sed mollis diam. Nunc aliquet risus ac finibus
-        porta. Nam quis arcu non lectus tincidunt fermentum. Suspendisse aliquet orci porta quam
-        semper imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus in. Sed
-        rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
-    </div>
+    <section>
+      <div id="about">
+        <div className="img-left">
+          <img
+            src="/assets/images/profile.jpg"
+            alt="profile dp"
+            className="about-img"
+          />
+        </div>
+        <div className="experience">
+          <div className="xp-cards">
+            <div className="card-template">
+              <span>
+                <HiBadgeCheck />
+              </span>
+              <h4>Experience</h4>
+              <p>Bootcamp training</p>
+            </div>
+            <div className="card-template">
+              <span>
+                <HiBadgeCheck />
+              </span>
+              <h4>Clients</h4>
+              <p>Upcoming</p>
+            </div>
+            <div className="card-template">
+              <span>
+                <HiBadgeCheck />
+              </span>
+              <h4>Projects</h4>
+              <p>15+</p>
+            </div>
+          </div>
+          <div>
+            <p className="about-text">
+              this is just a dummy text to be reviewed later for a more accurate
+              text that makes much mire more and morer sense lool this is just a
+              dummy text to be reviewed later for a more accurate text that
+              makes much mire more and morer sense lool this is just a dummy
+              text to be reviewed later for a more accurate text that makes much
+              mire more and morer sense lool
+            </p>
+            <button className="btn btn-primary mb-2">Let's talk</button>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
